6ff3f3354590a98ee4e8e19d9fe733dece589d80
Matthew Tondreau <mmtondreau@gmail.com>
2023-11-15 22:49:34 -0800

    Use dataclasses. Fix residual conv2d padding

--- torch_diffusion/model/context_unit.py
+++ torch_diffusion/model/context_unit.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import torch.nn as nn
 import pytorch_lightning as pl
 import torch
@@ -8,9 +9,16 @@ from torch_diffusion.model.unet_up import UnetUp
 from typing import List, TypedDict
 
 
-class ContextUnitConfig(TypedDict):
+@dataclass
+class ContextUnitLayerCOnfig:
+    kernel_size: int
     features: int
-    scale: list[int]
+
+
+@dataclass
+class ContextUnitConfig:
+    features: int
+    layers: list[ContextUnitLayerCOnfig]
 
 
 class ContextUnet(pl.LightningModule):
@@ -18,45 +26,51 @@ class ContextUnet(pl.LightningModule):
         super(ContextUnet, self).__init__()
 
         self.in_channels = in_channels
-        self.n_feat = config["features"]
-        self.scale = config["scale"]
-        num_layers = len(self.scale)
+        self.n_feat = config.features
+        self.layers = config.layers
+        num_layers = len(self.layers)
         self.h = height
         self.w = width
 
         self.init_conv = ResidualConvBlock(in_channels, self.n_feat, is_res=True)
 
-        input_dim = (2**num_layers) * self.n_feat
         feature_kernel = (height // (2**num_layers), width // (2**num_layers))
+
+        up0_dim = self.layers[-1].features
         self.up0 = nn.Sequential(
             nn.ConvTranspose2d(
-                input_dim,
-                input_dim,
+                up0_dim,
+                up0_dim,
                 feature_kernel,
                 feature_kernel,
             ),
-            nn.GroupNorm(8, input_dim),
+            nn.GroupNorm(8, up0_dim),
             nn.ReLU(),
         )
 
         self.down = nn.ModuleList()
         self.timeembed = nn.ModuleList()
         self.up = nn.ModuleList()
-        for layer, kernel in enumerate(self.scale):
+        prev_layer: int = self.n_feat
+        for layer in self.layers:
+            features = layer.features
             down = UnetDown(
-                (2**layer) * self.n_feat, (2 ** (layer + 1)) * self.n_feat, kernel
+                prev_layer,
+                features,
+                layer.kernel_size,
             )
-            embed = EmbedFC(1, 2 ** (layer + 1) * self.n_feat)
+            embed = EmbedFC(1, features)
 
             up = UnetUp(
-                (2 ** (layer + 2)) * self.n_feat,
-                (2**layer) * self.n_feat,
-                kernel_size=kernel,
+                2 * features,  # takes down cat embed as inputs
+                prev_layer,
+                kernel_size=layer.kernel_size,
             )
 
             self.timeembed.append(embed)
             self.down.append(down)
             self.up.append(up)
+            prev_layer = features
 
         self.to_vec = nn.Sequential(nn.AvgPool2d(feature_kernel), nn.GELU())
 
